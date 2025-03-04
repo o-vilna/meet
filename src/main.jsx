e916a2e123f9194a21108978760ff5240deb1f45
o-vilna <162020673+o-vilna@users.noreply.github.com>
2025-03-04 15:03:23 +0100

    Integrate Atatus for app monitoring and test setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as atatus from 'atatus-spa';
+
 import './index.css'
 import App from './App.jsx'
+
+atatus.config('1cdb7d6760df41cc836756b913328257').install();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
