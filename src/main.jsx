be8c12595c3de72f5ea057850bd9da79a6c856cd
o-vilna <162020673+o-vilna@users.noreply.github.com>
2025-03-10 13:02:35 +0100

    fix error NProgres is not defined

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,16 @@ import App from './App.jsx'
 import './index.css'
 // import * as atatus from 'atatus-spa';
 import { registerSW } from 'virtual:pwa-register';
-
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 // atatus.config('1b1ee5665eb943e3bdb71259beabe47a').install();
+
+//NProgress
+NProgress.configure({ 
+  showSpinner: false,
+  minimum: 0.1
+});
 
 // Додайте реєстрацію service worker через vite-plugin-pwa
 const updateSW = registerSW({
